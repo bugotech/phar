b060bc7d6db53d38af5c701f47794748200cbf5c
Bruno Goncalves <bugotech@gmail.com>
2017-05-17 00:50:59 -0300

    helpers

---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+if ( ! function_exists('work_path'))
+{
+    /**
+     * Get the path to the work with source or phar.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function work_path($path = '')
+    {
+        return getcwd() . ($path ? '/'.$path : $path);
+    }
+}
