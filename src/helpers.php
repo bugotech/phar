5cf6d5e51196b8f87bfd41fe944eb761c46542c8
Bruno Goncalves <bugotech@gmail.com>
2017-05-17 01:27:10 -0300

    ajustes stye 5

--- src/helpers.php
+++ src/helpers.php
@@ -1,14 +1,14 @@
 <?php
 
-if (! function_exists('work_path'))
-{
+if (! function_exists('work_path')) {
     /**
      * Get the path to the work with source or phar.
      *
      * @param  string  $path
      * @return string
      */
-    function work_path($path = '') {
+    function work_path($path = '')
+    {
         return getcwd() . ($path ? '/' . $path : $path);
     }
 }
