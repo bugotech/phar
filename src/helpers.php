7db46dcf754a359166e247e6b9cf953b8d661274
Bruno Goncalves <bugotech@gmail.com>
2017-06-14 09:34:12 -0300

    bin_path - style

--- src/helpers.php
+++ src/helpers.php
@@ -25,6 +25,7 @@ if (! function_exists('bin_path')) {
         global $_SERVER;
 
         $dir = files()->path($_SERVER['SCRIPT_FILENAME']);
+
         return $dir . ($path ? '/' . $path : $path);
     }
 }
