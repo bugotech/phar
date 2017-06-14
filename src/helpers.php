39b3924befb87f9642a36ec3d04c84ed8ada2c9e
Bruno Goncalves <bugotech@gmail.com>
2017-06-14 09:32:32 -0300

    bin_path

--- src/helpers.php
+++ src/helpers.php
@@ -12,3 +12,19 @@ if (! function_exists('work_path')) {
         return getcwd() . ($path ? '/' . $path : $path);
     }
 }
+
+if (! function_exists('bin_path')) {
+    /**
+     * Get the path to the work execute or phar.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function bin_path($path = '')
+    {
+        global $_SERVER;
+
+        $dir = files()->path($_SERVER['SCRIPT_FILENAME']);
+        return $dir . ($path ? '/' . $path : $path);
+    }
+}
